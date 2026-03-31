32f3bca979a88fb08e6adcf6752c8c9bdbc02454
Emil <iemil0906@gmail.com>
2026-03-31 15:16:54 +0300

    fix Field redeclare, add CORS and production API URL

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const cors = require("cors")
 const routes = require("./routes/router")
+const connectDB = require("./config/db") 
 
 const app = express()
+
+connectDB() 
 
 app.use(express.json())
 
 const allowedOrigins = [
-    process.env.REACT_APP_API_URL,
+    "https://hotel-aggregator-sigma.vercel.app",
     "http://localhost:3000"
 ]
 
@@ -15,14 +18,20 @@ app.use(cors({
     origin: function(origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true)
-    }
-    else {
-        callback(new Error("Not allowed by CORS"))
-    }
-},
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
     credentials: true
 }))
 
 app.use("/api", routes)
 
 module.exports = app
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = 5000
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT}`)
+    })
+}
--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-const PORT = 5000
-const connectDB = require("./config/db")
-
-connectDB()
-
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-}) 
