3c675c72856f32e888727e35011c938b4e653349
Emil <iemil0906@gmail.com>
2026-03-31 21:04:24 +0300

    fix Field redeclare, add CORS and production API URL

--- api/get-hotels.js
+++ api/get-hotels.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
     });
 
     res.status(200).json(response.data.data);
+    console.log("API_KEY:", process.env.API_KEY);
   } catch (err) {
     console.error(err.response?.data || err.message);
     res.status(500).json({ message: "Server error" });
