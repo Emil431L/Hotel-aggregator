59e38d4fb81e085cab4ad79ba490e43e0b9a4b9e
Emil <iemil0906@gmail.com>
2026-04-01 17:41:29 +0300

    fix Field redeclare, add CORS and production API URL

--- api/get-hotels.js
+++ api/get-hotels.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { kv } from "@vercel/kv"
 
 export default async function handler(req, res) {
   const { city } = req.query;
@@ -7,7 +8,16 @@ export default async function handler(req, res) {
     return res.status(400).json({ message: "City required" });
   }
 
+  const cacheKey = `hotels:${city.toLowercase().trim()}`
+
   try {
+    const cacheHotels = await kv.get(cacheKey)
+
+    if (cacheHotels) {
+      console.log("City ${city} returned from cache Vercel KV 🚀")
+      return res.status(200).json(cacheHotels)
+    }
+
     const response = await axios.get("https://api.hotels-api.com/v1/hotels/search", {
       params: { city, limit: 10 },
       headers: {
@@ -15,9 +25,14 @@ export default async function handler(req, res) {
       },
     });
 
-    res.status(200).json(response.data.data);
+    const hotels = response.data.data
+
+    await kv.set(cacheKey, hotels, { ex: 1296000 })
+
+    res.status(200).json(hotels);
   } catch (err) {
     console.error(err.response?.data || err.message);
     res.status(500).json({ message: "Server error" });
   }
 }
+
