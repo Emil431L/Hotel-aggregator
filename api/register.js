47b723f9208609d52e0066eb2d3c41ec80bee47f
Emil <iemil0906@gmail.com>
2026-04-25 15:51:14 +0300

    fix Field redeclare, add CORS and production API URL

--- api/register.js
+++ api/register.js
@@ -1,6 +1,6 @@
 import { connectDB } from "../lib/db"
 import { User } from "../models/User"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 
 export async function register (req, res) {
